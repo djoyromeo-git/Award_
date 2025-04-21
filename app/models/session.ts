bf475fe5513198cceeeb917c4584f3b58462a985
Djoy Djoy Mpongo Alex <djoyromeo@gmail.com>
2025-04-21 01:33:42 +0100

    Fix error in declaration for session model

--- app/models/session.ts
+++ app/models/session.ts
@@ -5,6 +5,21 @@ export default class Session extends BaseModel {
   @column({ isPrimary: true })
   declare id: number
 
+  @column()
+  declare name: string
+
+  @column()
+  declare description: string
+
+  @column.dateTime()
+  declare dateStart: DateTime
+
+  @column.dateTime()
+  declare dateEnd: DateTime
+
+  @column()
+  declare is_current: Boolean
+
   @column.dateTime({ autoCreate: true })
   declare createdAt: DateTime
 
